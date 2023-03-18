b6347f5716b06e967fbf5ea43a8abc4c8a155bb2
Iwona Malkinska <iwonamalkinska@Iwonas-MacBook-Air.local>
2023-03-18 08:00:21 +0000

    updated js questions

--- starter/assets/questions.js
+++ starter/assets/questions.js
@@ -1,4 +1,53 @@
-/*create an array consisting of questions and answers*/
+// JavaScript questions 
 
-const questions = []
+const codeQuiz = [
+  {
+      question: "Javascript is an _______ language?",
+      options: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+      answer: "Object-Oriented",
+  },
 
+  {
+      question: "Which of the following keywords is used to define a variable in Javascript?",
+      options: ["var", "let", "All of above", "None of the above"],
+      answer: "All of Above",
+  },
+
+  {
+      question: "Both var and let keywords are used to define a variable in Javascript?",
+      options: ["getElementbyId()", "getElementsByClassNAme()", "All of above", "None of the above"],
+      answer: "All of above",
+  },
+
+  {
+      question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+      options: ["Throws and error", "Ignores the statements", "Gives a warning", "None of the above"],
+      answer: "Ignores the statements",
+  },
+
+  {
+      question: "Which of the following methods can be used to display data in some form using Javascript?",
+      options: ["document.write()", "console.log()", "windows.alert()", "All of the above"],
+      answer: "All of the above",
+  },
+  {
+    question: "How do you write " + '"Hello World" ' + "in an alert box?",
+    options: ['alert("Hello World")', 'msg("Hello World")', 'msgBox("Hello World")', 'alertBox("Hello World")'],
+    answer: 'alert("Hello World")'
+},
+{
+  question: "How do you write an IF statement in JavaScript?",
+  options: ["if i === 5 then", "if i = 5 ()", "if (i = 5) then", "if (i === 5) {}"],
+  answer: "if (i === 5) {}"
+},
+{
+  question: "Which operator is used to assign a value to a variable?",
+  options: ["=", "x", "-", "*"],
+  answer: "="
+},
+{
+  question: "Which of the following is not a looping structure?",
+  options: ["For", "While", "ForEach", "Goto"],
+  answer: "ForEach"
+},
+];
--- starter/assets/scores.js
+++ starter/assets/scores.js
@@ -1,5 +1,25 @@
-//document selecting ID's
-let highscoresElement = document.querySelector("#highscores");
-let clear = document.querySelector("#clear");
+// Assigning highscores element to variable
+const highscoresEl = $('#highscores');
+const clearButtonEl = $('#clear');
 
-let highscores = [];
+// Function to get scores array from local storage then iterate through this and create a new list element for each object. This list item then shows the details we want from each object.
+function displayScores() {
+    scores = JSON.parse(localStorage.getItem("leaderboard") || "[]");
+    scores.forEach(element => {
+        var userScoreEl = $('<li>');
+        userScoreEl.text("Initials: " + element.initials + " Final Score: " + element.finalScore + " Time Remaining: " + element.finalTime);
+        highscoresEl.append(userScoreEl);
+    });
+}
+
+displayScores();
+
+// Function to clear screen of scores and delete scores array from local storage
+
+function clearScores() {
+    highscoresEl.empty();
+    localStorage.clear();
+}
+
+// Add event listener to clear button that links to function
+clearButtonEl.on('click', clearScores);
